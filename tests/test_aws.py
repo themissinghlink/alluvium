c6943b0581e66aaa2ba4359b178d37556e154ce1
Abhinava Singh <abhinavasingh@Aanchals-MacBook-Air.local>
2020-06-18 17:35:30 -0700

    lint fixes

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,16 +1,15 @@
+import json
 import os
+from tempfile import TemporaryFile
 
 import boto3
 import pytest
-import json
-from tempfile import TemporaryFile
-
 from moto import mock_s3
 
 
 @pytest.fixture
 def bucket_name():
-    return 'fake_bucket'
+    return "fake_bucket"
 
 
 @pytest.fixture
@@ -19,18 +18,18 @@ def mock_bucket(bucket_name):
         client = boto3.resource("s3")
         client.create_bucket(Bucket=bucket_name)
         fake_bucket = client.Bucket(bucket_name)
-        
-        for val in ['bar', 'baz', 'qux']:
-            fake_file_contents = {'foo': val}
-            file_name = '{}.json'.format(val)
+
+        for val in ["bar", "baz", "qux"]:
+            fake_file_contents = {"foo": val}
+            file_name = "{}.json".format(val)
             key = client.Object(bucket_name, file_name)
-            with open(file_name, 'w+') as fp:
+            with open(file_name, "w+") as fp:
                 json.dump(fake_file_contents, fp)
-            with open(file_name, 'r+b') as fp:
+            with open(file_name, "r+b") as fp:
                 key.put(Body=fp)
             os.remove(file_name)
         yield fake_bucket
 
 
 def test_get_keys_with_prefix(mock_bucket):
-    assert [o.key for o in mock_bucket.objects.all()] == ['bar.json', 'baz.json', 'qux.json']
+    assert [o.key for o in mock_bucket.objects.all()] == ["bar.json", "baz.json", "qux.json"]
